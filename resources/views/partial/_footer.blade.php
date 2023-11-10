a1a7bf36cce1cf0e3939e455393a41da8c77fe4f
akashtripathiiiii <akashtripathi0108@gmail.com>
2023-11-10 19:36:50 +0530

    initial commit

---
+++ resources/views/partial/_footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+  <div class="float-right d-none d-sm-inline-block">
+    &copy; 2023 Developed and Designed with Laravel <div class="fas fa-heart"></div>  <a href="https://github.com/akashtripathiiiii" class="btn-link" target="_blank">Akash Tripathi</a>. All rights reserved.
+  </div>
+</footer>
+</div>
